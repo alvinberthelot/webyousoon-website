21b16cd3754663ff1dbfde6b7301f64c30af8fa8
Alvin Berthelot <alvin@Alvins-MacBook-Air.local>
2020-04-11 08:29:04 +0200

    chore: Split content for an employee page

--- src/employees/alvin-berthelot/alvin-berthelot.11tydata.js
+++ src/employees/alvin-berthelot/alvin-berthelot.11tydata.js
@@ -1,14 +1,91 @@
 module.exports = {
     "firstname": "Alvin",
     "lastname": "Berthelot",
-    "skills": [
-        {
-            "title" : "Tech lead / développeur senior",
-            "subskills" : [
-                "Encadrement technique",
-                "Développement front-end",
-                "Développement back-end",
-            ]
-        }
-    ]
+    "skillsSection": {
+        "title": "Compétences",
+        "description": "Je propose des services dans toutes les étapes clés pour réaliser des applications web et mobiles de qualité.",
+        "skills": [
+            {
+                "title" : "Tech lead / développeur senior",
+                "subskills" : [
+                    "Encadrement technique",
+                    "Développement front-end",
+                    "Développement back-end",
+                ]
+            }
+        ]
+    },
+    "technicalSkillsSection": {
+        "title": "Environnements techniques",
+        "description": "Des connaissances larges sur les principales technologies autour de l'industrialisation web.",
+        "aggregates": [
+            {
+                "title": "front-end",
+                "stacks" : [
+                    {
+                        "title" : "Web",
+                        "substacks" : [
+                            {
+                                "title": "HTML",
+                                "knowledges" : [
+                                    "HTML5",
+                                    "Adaptive web design",
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+
+        ]
+    },
+    "educationSection": {
+        "title": "Formations",
+        "description": "Mon objectif c'est de faire au minimum 2 formations par an.",
+        "years": [
+            {
+                "title": "2019",
+                "educations" : ["tutu", "titi"]
+            }
+
+        ]
+    },
+    
+    "workExperienceSection": {
+        "title": "Expériences",
+        "description": "",
+        "missions": [
+            {
+                "company": "CBP",
+                "dateBegin": "23-04-2018",
+                "dateEnd": null,
+                "place": "Nantes",
+                "title": "Encadrement et développement Angular",
+                "description": "Expert technique Angular au sein de l'équipe CBP pour la mise en place d'une architecture front-end avec encadrement de l'équipe interne pour une montée en compétences",
+                "tasks": [
+                    "Mise en place de l'architecture front-end Angular",
+                    "Encadrement technique de l'équipe interne",
+                    "Mise en place de l'outillage et de l'industrialisation"
+                ],
+                "tags" : ["TypeScript", "Angular", "ngRx", "ngBootstrap", "Node.js", "HTML5", "CSS3", "Sass", "Travis", "Git"],
+                "repository": null,
+                "downloads": null
+            }
+
+        ]
+    },
+
+    "eventsSection": {
+        "title": "Conférences",
+        "description": "",
+        "events": [
+            {
+                "title": "Agile Tour Nantes",
+                "date": "23-04-2018",
+                "session": null,
+                "website": null
+            }
+
+        ]
+    }  
 }
