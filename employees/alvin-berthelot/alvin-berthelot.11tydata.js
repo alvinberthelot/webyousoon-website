dc7168cea05ec034c2926c45c3fd6a134e4e4ccb
Alvin Berthelot <alvin@Alvins-MacBook-Air.local>
2020-04-11 05:23:27 +0200

    chore: Using data with JS  to build page

---
+++ employees/alvin-berthelot/alvin-berthelot.11tydata.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "firstname": "Alvin",
+    "lastname": "Berthelot",
+    "skills": [
+        {
+            "title" : "Tech lead / développeur senior",
+            "subskills" : [
+                "Encadrement technique",
+                "Développement front-end",
+                "Développement back-end",
+            ]
+        }
+    ]
+}
