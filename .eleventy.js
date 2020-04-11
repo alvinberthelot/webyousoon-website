d87d6f764e9138e9923f608f432b7d1fed605c44
Alvin Berthelot <alvin@Alvins-MacBook-Air.local>
2020-04-11 09:17:12 +0200

    chore: Add HTML minification for production

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
 module.exports = function (eleventyConfig) {
+  // minify the html output when running in prod
+  if (process.env.NODE_ENV == "production") {
+    eleventyConfig.addTransform(
+      "htmlmin",
+      require("./src/utils/minify-html.js")
+    )
+  }
+
   eleventyConfig.setTemplateFormats(["njk"])
   eleventyConfig.addPassthroughCopy("src/robots.txt")
 }
