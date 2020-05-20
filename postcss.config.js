3e317a51f6e67d4c31422539fb9ab271831736e4
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2020-05-20 08:44:03 +0200

    chore: Purge and minify output CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,24 @@
+const tailwindcss = require("tailwindcss")("tailwind.config.js")
+
+const autoprefixer = require("autoprefixer")
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/**/*.html", "./src/**/*.njk"],
+  defaultExtractor: (content) => {
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+    return broadMatches.concat(innerMatches)
+  },
+})
+
+const cssnano = require("cssnano")({
+  preset: "default",
+})
+
 module.exports = {
   plugins: [
-    require("tailwindcss")("tailwind.config.js"),
-    require("autoprefixer"),
+    tailwindcss,
+    autoprefixer,
+    ...(process.env.NODE_ENV === "production" ? [purgecss, cssnano] : []),
   ],
 }
