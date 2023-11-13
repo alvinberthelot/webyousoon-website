b98892cd45ba2276b7ade1aaf8b5fd76d5a05ba6
Alvin Berthelot <alvin.berthelot+github@webyousoon.com>
2023-11-13 19:55:34 +0100

    fix(TS): Fix types to script "astro check"

--- src/types/employee.types.ts
+++ src/types/employee.types.ts
@@ -1,4 +1,14 @@
-export type Skill = {};
+export type Subskill = {
+  title: string;
+  period: string;
+  subskills: [];
+};
+
+export type Skill = {
+  title: string;
+  period: string;
+  subskills: Subskill[];
+};
 
 export type SkillsSection = {
   title: string;
@@ -6,12 +16,30 @@ export type SkillsSection = {
   skills: Skill[];
 };
 
-export type Aggregate = {};
+export type Substack = {
+  title: string;
+  knowledges: string[];
+};
+
+export type Stack = {
+  title: string;
+  substacks: Substack[];
+};
+
+export type Aggregate = {
+  title: string;
+  stacks: Stack[];
+};
 
 export type TechnicalSkillsSection = {
   title: string;
   description: string;
   aggregates: Aggregate[];
+};
+
+export type EducationsByYear = {
+  year: string;
+  educations: Education[];
 };
 
 export type Education = {
@@ -24,18 +52,41 @@ export type EducationsSection = {
   title: string;
   description: string;
   educations: Education[];
+  educationsByYear: Education[];
+};
+
+export type Tag = {
+  tag: string;
+  wording: string;
 };
 
 export type Experience = {
+  id: string;
   title: string;
+  company: string;
+  description: string;
+  tasks: string[];
+  place: string;
   date: string;
-  tags: string[];
+  tags: Tag[];
+  repository: string;
+  downloads: string;
+  // dateBegin: "2007-01-01";
+  // dateEnd: "2007-05-01";
+
+  // duration: [Object];
+  // scores: [Object];
 };
 
 export type ExperiencesSection = {
   title: string;
   description: string;
   experiences: Experience[];
+};
+
+export type EventsByYear = {
+  year: string;
+  events: Event[];
 };
 
 export type Event = {
@@ -48,14 +99,15 @@ export type EventsSection = {
   title: string;
   description: string;
   events: Event[];
+  eventsByYear: EventsByYear[];
 };
 
 export type Employee = {
   version: string;
-  firstName: string;
-  lastName: string;
+  firstname: string;
+  lastname: string;
   skillsSection: SkillsSection;
-  technicalskillsSection: TechnicalSkillsSection;
+  technicalSkillsSection: TechnicalSkillsSection;
   educationsSection: EducationsSection;
   experiencesSection: ExperiencesSection;
   eventsSection: EventsSection;
